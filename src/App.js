bd5b052fe2f47c81ebd7f0cfce3026211a9d6311
Brandon Griffith <BrandonGri5th@gmail.com>
2019-01-03 12:16:35 -0700

    searchparams

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import Results from "./Results";
-import { Router } from "@reach/router";
 import Details from "./Details";
-import { Link } from "@reach/router";
+import SearchParams from "./SearchParams";
 
 class App extends React.Component {
   render() {
@@ -15,6 +15,7 @@ class App extends React.Component {
         <Router>
           <Results path="/" />
           <Details path="/details/:id" />
+          <SearchParams path="/search-params" />
         </Router>
       </div>
     );
